c746029e5e5570eb945a6369b374f4144024aea7
masawong <masawong@163.com>
2015-10-05 13:34:48 +0800

    SHSwitchView

---
+++ app/src/main/java/mw/ankara/uikit/AnSwitch.java
@@ -0,0 +1,24 @@
+package mw.ankara.uikit;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.sevenheaven.iosswitch.ShSwitchView;
+
+/**
+ * @author masawong
+ * @since 10/5/15
+ */
+public class AnSwitch extends ShSwitchView {
+    public AnSwitch(Context context) {
+        super(context);
+    }
+
+    public AnSwitch(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AnSwitch(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
